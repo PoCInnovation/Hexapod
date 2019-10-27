23b27cf95115ab9b789a55651d7d2dc156df1877
Yohann <yohannassouline@hotmail.fr>
2019-10-27 16:59:11 +0100

    fix some bugs and add an abstraction fonction to move engines

--- Client/hardcoded_movements.py
+++ Client/hardcoded_movements.py
@@ -2,87 +2,77 @@ from values import *
 import sys
 import time
 
+ENGINES = {
+    "rear_r_hori" : REAR_R_HORI,
+    "rear_r_vert" : REAR_R_VERT,
+    "rear_r_knee" : REAR_R_KNEE,
+    "rear_l_hori" : REAR_L_HORI,
+    "rear_l_vert" : REAR_L_VERT,
+    "rear_l_knee" : REAR_L_KNEE,
+    "midd_r_hori" : MIDD_R_HORI,
+    "midd_r_vert" : MIDD_R_VERT,
+    "midd_r_knee" : MIDD_R_KNEE,
+    "midd_l_hori" : MIDD_L_HORI,
+    "midd_l_vert" : MIDD_L_VERT,
+    "midd_l_knee" : MIDD_L_KNEE,
+    "fron_r_hori" : FRON_R_HORI,
+    "fron_r_vert" : FRON_R_VERT,
+    "fron_r_knee" : FRON_R_KNEE,
+    "fron_l_hori" : FRON_L_HORI,
+    "fron_l_vert" : FRON_L_VERT,
+    "fron_l_knee" : FRON_L_KNEE
+}
+
 class Hexapod_movements:
     def __init__(self, connection):
         self.hexapod_connection = connection
+        self.movements_speed = 700
 
     def move_engine(self, articulation, deg, speed, tab, sleep_time):
-        # We need to know which side the engine is on
-        side = articulation <= 15
+        side = articulation <= 15           # We need to know which side the engine is on
         # Convert ratio 0-1 to the engine min-max value
         deg = deg * (tab[side][0] - tab[side][1]) + tab[side][1]
-        # Command to send to the LynxMotion board
-        command = "#%dP%.0fS%d!" % (articulation, deg, speed)
-        # #=pin
-        # P=position
-        # S=speed
-        # !=custom EOL for arduino decode
+        command = "#%dP%.0fS%d!" % (articulation, deg, speed)   # Command to send
+        #          #=pin P=position S=speed !=EOL for arduino decode
         self.hexapod_connection.send_command(command, sleep_time)
 
     def move_knee(self, articulation, deg, sleep):
-        self.move_engine(articulation, deg, 700, KNEE_VALUES, sleep)
+        self.move_engine(articulation, deg, self.movements_speed, KNEE_VALUES, sleep)
 
     def move_vert(self, articulation, deg, sleep):
-        self.move_engine(articulation, deg, 700, VERT_VALUES, sleep)
+        self.move_engine(articulation, deg, self.movements_speed, VERT_VALUES, sleep)
 
     def move_hori(self, articulation, deg, sleep):
-        self.move_engine(articulation, deg, 700, HORI_VALUES, sleep)
+        self.move_engine(articulation, deg, self.movements_speed, HORI_VALUES, sleep)
 
 class HardcodedMovements:
     def __init__(self, connection):
         self.hexapod = Hexapod_movements(connection)
         self.sleep_action_time = 0.25
 
+    def move_kind(self, kind, deg):
+        affected_engines = []
+        for key in ENGINES:
+            if kind in key:
+                affected_engines.append(ENGINES[key])
+        for engine in affected_engines:
+            getattr(self.hexapod, "move_" + kind)(engine, deg, 0)
+
     def sit(self):
-        self.hexapod.move_vert(FRON_L_VERT, 1, 0)
-        self.hexapod.move_vert(FRON_R_VERT, 1, self.sleep_action_time)
-        self.hexapod.move_vert(REAR_L_VERT, 1, 0)
-        self.hexapod.move_vert(REAR_R_VERT, 1, self.sleep_action_time)
-        self.hexapod.move_vert(MIDD_L_VERT, 1, 0)
-        self.hexapod.move_vert(MIDD_R_VERT, 1, self.sleep_action_time)
-
-        self.hexapod.move_knee(FRON_L_KNEE, 0, 0)
-        self.hexapod.move_knee(FRON_R_KNEE, 0, self.sleep_action_time)
-        self.hexapod.move_knee(REAR_L_KNEE, 0, 0)
-        self.hexapod.move_knee(REAR_R_KNEE, 0, self.sleep_action_time)
-        self.hexapod.move_knee(MIDD_L_KNEE, 0, 0)
-        self.hexapod.move_knee(MIDD_R_KNEE, 0, 1)
-
-        self.hexapod.move_hori(FRON_L_HORI, 0.5, 0)
-        self.hexapod.move_hori(FRON_R_HORI, 0.5, self.sleep_action_time)
-        self.hexapod.move_hori(REAR_L_HORI, 0.5, 0)
-        self.hexapod.move_hori(REAR_R_HORI, 0.5, self.sleep_action_time)
-        self.hexapod.move_hori(MIDD_L_HORI, 0.5, 0)
-        self.hexapod.move_hori(MIDD_R_HORI, 0.5, 0)
+        self.move_kind("vert", 1)
+        time.sleep(0.25)
+        self.move_kind("knee", 0)
+        time.sleep(0.25)
+        self.move_kind("hori", 0.5)
 
     def stand(self):
-        self.hexapod.move_hori(FRON_L_HORI, 0.5, 0)
-        self.hexapod.move_hori(FRON_R_HORI, 0.5, 0)
-        self.hexapod.move_hori(REAR_L_HORI, 0.5, 0)
-        self.hexapod.move_hori(REAR_R_HORI, 0.5, 0)
-        self.hexapod.move_hori(MIDD_L_HORI, 0.5, 0)
-        self.hexapod.move_hori(MIDD_R_HORI, 0.5, 0)
-
-        self.hexapod.move_knee(FRON_L_KNEE, 0.3, 0)
-        self.hexapod.move_knee(FRON_R_KNEE, 0.3, self.sleep_action_time)
-        self.hexapod.move_knee(REAR_L_KNEE, 0.3, 0)
-        self.hexapod.move_knee(REAR_R_KNEE, 0.3, self.sleep_action_time)
-        self.hexapod.move_knee(MIDD_L_KNEE, 0.3, 0)
-        self.hexapod.move_knee(MIDD_R_KNEE, 0.3, self.sleep_action_time)
-
-        self.hexapod.move_vert(MIDD_L_VERT, 0.4, 0)
-        self.hexapod.move_vert(MIDD_R_VERT, 0.4, self.sleep_action_time)
-        self.hexapod.move_vert(FRON_L_VERT, 0.4, 0)
-        self.hexapod.move_vert(FRON_R_VERT, 0.4, self.sleep_action_time)
-        self.hexapod.move_vert(REAR_L_VERT, 0.4, 0)
-        self.hexapod.move_vert(REAR_R_VERT, 0.4, self.sleep_action_time)
-
-        self.hexapod.move_knee(FRON_L_KNEE, 0.5, 0)
-        self.hexapod.move_knee(FRON_R_KNEE, 0.5, self.sleep_action_time)
-        self.hexapod.move_knee(REAR_L_KNEE, 0.5, 0)
-        self.hexapod.move_knee(REAR_R_KNEE, 0.5, self.sleep_action_time)
-        self.hexapod.move_knee(MIDD_L_KNEE, 0.5, 0)
-        self.hexapod.move_knee(MIDD_R_KNEE, 0.5, 0)
+        self.move_kind("hori", 0.5)
+        time.sleep(0.5)
+        self.move_kind("front", 0.3)
+        time.sleep(0.25)
+        self.move_kind("vert", 0.4)
+        time.sleep(0.25)
+        self.move_kind("knee", 0.5)
 
     def wave(self):
         self.hexapod.move_vert(FRON_R_VERT, 1, 0)
@@ -111,7 +101,7 @@ class HardcodedMovements:
         self.hexapod.move_vert(FRON_R_VERT, 0.3, self.sleep_action_time)
 
         time.sleep(2) # wait for a bit in dab position
-        # WTF why don't we call stand here ??
+        # self.stand() # <-- TO BE TESTED
 
         self.hexapod.move_vert(FRON_R_VERT, 1, self.sleep_action_time)
         self.hexapod.move_vert(FRON_L_VERT, 0.4, 0)
--- Client/main.py
+++ Client/main.py
@@ -16,26 +16,30 @@ class HexapodConnection:
         self.port = port
         self.init_connection()
 
-    def __exit__(self, type, value, traceback):
+    def close(self):
         self.socket.close()
 
     def init_connection(self):
         try:
             self.socket.connect((self.host, self.port))
         except:
-            print("You must connect to Hexapod's wifi first")
+            print("\nYou must connect to Hexapod's wifi first")
             exit(1)
 
     def send_command(self, command, sleep_time):
         command = bytes(command.encode('utf-8'))
-        self.socket.send(command)
+        try:
+            self.socket.send(command)
+        except:
+            print("\nError : couldn't send command")
+            self.close()
+            exit(1)
         time.sleep(sleep_time)
 
 
 class Hexapod:
     def __init__(self):
         self.connection = HexapodConnection(SOCKET_HOST, SOCKET_PORT)
-        # self.connection = 1 # debug
         self.hexapod_movements = HardcodedMovements(self.connection)
         self.valid_commands = ["help", "sit", "stand", "forward", "dab", "wave"]
         self.position = "sit"
@@ -48,19 +52,20 @@ class Hexapod:
         print("\n")
 
     def check_command_possibility(self, command):
+        initial_position = self.position
         if self.position == "sit":
             if command == "stand":
                 self.position = "stand"
             elif command == "dab" or command == "wave" or command == "forward":
                 self.do_action("stand")
                 time.sleep(1)
-                self.position = "stand"
-        if self.position == "stand":
+        elif self.position == "stand":
             if command == "sit":
                 self.position = "sit"
         self.do_action(command)
-
-
+        if self.position != initial_position:
+            if initial_position == "sit" and command != "stand":
+                self.do_action("sit")
 
     def start_prompt(self):
         print("Type help to see available commands")
@@ -69,6 +74,7 @@ class Hexapod:
             try:
                 command = input("$ ")
             except:
+                self.connection.close()
                 exit(0)
             if command not in self.valid_commands:
                 print("{} : Invalid command".format(command))
@@ -80,7 +86,7 @@ class Hexapod:
                 self.check_command_possibility(command)
 
     def do_action(self, command):
-        print("Command:", command) # debug
+        print("Command:", command)
         getattr(self.hexapod_movements, command)()
 
 if __name__ == '__main__':
