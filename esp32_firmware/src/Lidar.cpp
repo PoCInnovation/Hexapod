c2d0f163b796dc920769158c96738d00934e9b9f
Yohann <yohannassouline@hotmail.fr>
2021-01-09 23:53:31 +0100

    fix lidar start and stop

--- esp32_firmware/src/Lidar.cpp
+++ esp32_firmware/src/Lidar.cpp
@@ -1,6 +1,8 @@
 #include "Lidar.hpp"
 #include "lidar_protocol.h"
 
+#define START_BYTE 0xA5
+
 Lidar::Lidar() : _lidarSerial(115200)
 {
 
@@ -8,10 +10,12 @@ Lidar::Lidar() : _lidarSerial(115200)
 
 void Lidar::start()
 {
+    _lidarSerial.write(START_BYTE);
     _lidarSerial.write(LIDAR_CMD_SCAN);
 }
 
 void Lidar::stop()
 {
+    _lidarSerial.write(START_BYTE);
     _lidarSerial.write(LIDAR_CMD_STOP);
 }
