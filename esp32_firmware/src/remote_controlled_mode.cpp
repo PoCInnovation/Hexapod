7c1941f7dba906bd3fb3d768b58edbb03b5875a0
Yohann <yohannassouline@hotmail.fr>
2021-08-11 01:23:25 +0200

    update remote mode

--- esp32_firmware/src/remote_controlled_mode.cpp
+++ esp32_firmware/src/remote_controlled_mode.cpp
@@ -9,13 +9,71 @@ RemoteControlledMode::RemoteControlledMode(HexapodSerialController &hexapodSeria
 {
 }
 
+/*
+    Available commands are :
+
+        F -> Forward (until stop)
+        B -> Backward (until stop)
+        R -> Right (until stop)
+        L -> Left (until stop)
+        S -> Stop
+
+        TR -> Turn right
+        TL -> Turn left
+
+        U1 -> Up position 1
+        U2 -> Up position 2
+        U3 -> Up position 3
+
+        RST -> Reset position
+
+        HI -> Say hi with a leg
+        DAB -> Do a dab
+
+        SIT -> Sit down and turn motors off
+*/
+
 void RemoteControlledMode::bleRxCallback(std::string str)
 {
-    Serial.println("Received in cb:");
-    Serial.println(str.c_str());
-    Serial.println();
-    Serial.println("OIK");
-    // _hexapodSerialController.send(str.c_str());
+    if (str.size() == 1) {
+        switch (str[0]) {
+            case 'F':
+                // TODO
+                break;
+
+            case 'B':
+                // TODO
+                break;
+
+            case 'R':
+                // TODO
+                break;
+
+            case 'L':
+                // TODO
+                break;
+
+            case 'S':
+                // TODO
+                break;
+        }
+    } else if (str[0] == 'T') {  // turn
+        if (str[1] == 'R') {
+            // TODO
+        } else if (str[1] == 'L') {
+            // TODO
+        }
+    } else if (str[0] == 'U') {
+        // TODO
+    } else if (str == "RST") {
+        // TODO
+    } else if (str == "HI") {
+        // TODO
+    } else if (str == "DAB") {
+        // TODO
+    } else if (str == "SIT") {
+        // TODO
+    }
 }
 
 void RemoteControlledMode::setup()
